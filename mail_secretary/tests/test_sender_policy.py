621366327764927fee96391c97f82b675a5afdbe
None <yumens2@gmail.com>
2026-04-28 00:08:26 +0900

    Update test_sender_policy.py

--- mail_secretary/tests/test_sender_policy.py
+++ mail_secretary/tests/test_sender_policy.py
@@ -2,10 +2,10 @@ from app.mail.sender_policy import is_allowed_sender
 
 
 def test_allowed_sender_accepts_case_insensitive_address() -> None:
-    allowed = {"yumens2@gmail.com", "jaewon.yu@hcs.com"}
+    allowed = {"", ""}
     assert is_allowed_sender("Yu <Jaewon.Yu@hcs.com>", allowed)
 
 
 def test_allowed_sender_rejects_unknown_sender() -> None:
-    allowed = {"yumens2@gmail.com"}
+    allowed = {""}
     assert not is_allowed_sender("bad@example.com", allowed)
